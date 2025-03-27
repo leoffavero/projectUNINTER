63906e2e368af917c4ee5c0cd00b2d7ab059453a
leoffavero <leonardoffavero@gmail.com>
2025-03-27 18:37:07 -0300

    player move

--- code/player.py
+++ code/player.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import pygame
 
-from code.Const import ENTITY_SPEED
+from code.Const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH
 from code.entity import Entity
 
 
@@ -14,4 +14,10 @@ class Player(Entity):
         pressed_key = pygame.key.get_pressed()
         if pressed_key[pygame.K_UP] and self.rect.top > 0:
             self.rect.centery -= ENTITY_SPEED[self.name]
+        if pressed_key[pygame.K_DOWN] and self.rect.bottom < WIN_HEIGHT:
+            self.rect.centery += ENTITY_SPEED[self.name]
+        if pressed_key[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+        if pressed_key[pygame.K_RIGHT] and self.rect.right < WIN_WIDTH:
+            self.rect.centerx += ENTITY_SPEED[self.name]
         pass
