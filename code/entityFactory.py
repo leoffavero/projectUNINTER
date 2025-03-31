9c923efec4d3cc06f467226c2da21420b45d4ffa
leoffavero <leonardoffavero@gmail.com>
2025-03-31 13:37:25 -0300

    correção troca de level e enemy. falta dano

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -12,20 +12,23 @@ from code.player import Player
 class entityFactory:
 
     @staticmethod
-    def get_entity(entity_name: str, position=(0,0)):
+    def get_entity(entity_name: str, position=(0, 0)):
+
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
-                for i in range (7):
+                for i in range(7):
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+
             case 'Level2Bg':
                 list_bg = []
-                for i in range(7):
+                for i in range(5):  # Level2Bg0 até Level2Bg4
                     list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
             case 'Player2':
@@ -33,4 +36,5 @@ class entityFactory:
             case 'Enemy1':
                 return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
             case 'Enemy2':
-                return Enemy('Enemy2',(WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
+
